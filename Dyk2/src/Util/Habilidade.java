25a35082fca36af90a95102d83a7023977245c53
Thiago Felipe Dos Santos <thiagonfss@gmail.com>
2021-12-03 19:52:09 -0300

    Alterações nas habilidades

--- Dyk2/src/Util/Habilidade.java
+++ Dyk2/src/Util/Habilidade.java
@@ -5,11 +5,13 @@ public class Habilidade
 {
     private int CodigoHabilidade;
     private String Habilidade;
+    private boolean habilidadeUtilizada;
 
-    public Habilidade(int CodigoHabilidade, String Habilidade)
+    public Habilidade(int CodigoHabilidade, String Habilidade, boolean habilidadeUtilizada)
     {
         this.CodigoHabilidade = CodigoHabilidade;
         this.Habilidade = Habilidade;
+        this.habilidadeUtilizada = habilidadeUtilizada;
     }
 
     public int getCodigoHabilidade()
