58a92e59be1072b1e157f01d8e65bd0f4a55e0c4
Thiago Felipe Dos Santos <thiagonfss@gmail.com>
2021-12-03 01:41:10 -0300

    alteração na logica de disposição do jogo

--- Dyk2/src/dyk2/Dyk2.java
+++ Dyk2/src/dyk2/Dyk2.java
@@ -5,8 +5,10 @@ import Util.Menu;
 import java.util.Scanner;
 import Util.Usuario;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
-public class Dyk2 {
+public class Dyk2 
+{
     //constantes
     public static final int SIM = 1;
     public static final int NAO = 2;
@@ -17,6 +19,8 @@ public class Dyk2 {
     static Menu menu = new Menu();
     static JDBCConector conector = new JDBCConector();
     static Usuario usuario = new Usuario();
+    static ArrayList<Usuario> usuariosLogados = new ArrayList<Usuario>();
+    
     
     public static void main(String[] args) throws SQLException 
     {
@@ -37,22 +41,94 @@ public class Dyk2 {
             {
                 case 1 -> iniciaJogo();
                 case 2 -> System.out.println("opcao2");
-                case 3 -> {
+                case 3 -> 
+                {
                     menu.MenuRegras();
                     System.out.println("\n");
                     menu.MenuInicializacao();
                 }
-                case 0 -> menu.MenuEncerramento();
+                case 4 ->
+                    {
+                        System.out.println("cadastrar perguntas");//TODO: Logica
+                    }
+                case 0 -> 
+                {
+                    menu.MenuEncerramento();
+                    conector.encerrarConexao();
+                    System.exit(0);
+                }
                 default -> {
                     System.out.println("\n Opção inválida digite novamente!");
-                    menu.MenuInicial();
+                    menu.MenuInicializacao();
                 }
             }
         }
         while (opcaoInicial >1 || opcaoInicial<0);
-        conector.encerrarConexao();
         if (opcaoInicial == 1)
             System.out.println("Fim de jogo!");
+        
+        conector.encerrarConexao();
+    }//falta preenche o item 2 do menu
+    
+    
+    
+    public static void iniciaJogo() throws SQLException
+    {
+        System.out.println("Lembrando que o DYK é um jogo para jogar em duplas!\n");
+        
+        for (int i = 1; i <= QUANTIDADE_JOGADORES; i++){
+            
+            System.out.println("Jogador n° " + i + ", seja bem vindo!!!\n");
+            
+            boolean opcaoValida = false;
+            do 
+            {
+                menu.MenuInicial();
+                System.out.print("Digite a opção desejada: ");
+                int opcaoDesejada = in.nextInt();
+                
+                switch (opcaoDesejada){
+                    case 1 ->//Usuario cadastrado
+                    {
+                        Usuario jogador = buscarJogador();//TODO: Logica
+                                                
+                        if (jogador.getCodigoUsuario()>0)
+                        {
+                            jogador.setNumeroJogador(i);
+                            usuariosLogados.add(jogador);
+                            System.out.println("Usuario logado com sucesso!");
+                            opcaoValida = true;
+                        }
+                        
+                    }
+                    case 2 ->
+                    {
+                        System.out.println("chamar metodo para ja cadastrado");//TODO: Logica
+                        opcaoValida = true;
+                    }
+                    case 3 ->
+                    {
+                        System.out.println("chamar metodo para ranking");//TODO: Logica
+                        opcaoValida = true;
+                    }
+                    case 0 -> 
+                    {
+                        menu.MenuEncerramento();
+                        conector.encerrarConexao();
+                        System.exit(0);
+                    }
+                    default -> 
+                    {
+                        System.out.println("\n Opção inválida digite novamente!");
+                    }
+                }
+            }
+            while (!opcaoValida);
+            
+        }
+        
+        //inicia o jogo(passando a lista de usuarios)
+                
     }
     
     public static int validacaoSimNao()
@@ -68,30 +144,19 @@ public class Dyk2 {
         return validacao; 
     }
     
-    public static void iniciaJogo() throws SQLException
-    {
-        System.out.print("""
-                         Lembrando que o DYK é um jogo para jogar em duplas!  
-                           """);
+    public static Usuario buscarJogador() throws SQLException
+    {            
+        System.out.print("Digite o seu e-mail: ");
+        String email = in.next();
+        usuario.setEmail(email);
+
+        System.out.print("Digite sua senha: ");
+        String senha = in.next();
+        usuario.setSenha(senha);
         
-        for (int i = 1; i <= QUANTIDADE_JOGADORES; i++){
-            System.out.println("Jogado n° "+ i + "");
-        }
-        int existeUsuarioCadastrado = validacaoSimNao();
-        
-        if(existeUsuarioCadastrado == SIM)
-        {
-            System.out.print("Digite o seu e-mail: ");
-            String email = in.next();
-            usuario.setEmail(email);
-            
-            System.out.print("Digite sua senha: ");
-            String senha = in.next();
-            usuario.setSenha(senha);
-            
-            Usuario teste = conector.buscarUsuario(email,senha);
-        }
-        
+        // Recupero o Personagem do usuário do banco
+
+        return conector.buscarUsuario(email,senha);
     }
 }
 
