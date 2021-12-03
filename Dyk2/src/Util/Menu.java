cf164b76d135b89036b8b6142f0ae19762da8cd9
Thiago Felipe Dos Santos <thiagonfss@gmail.com>
2021-12-03 01:39:23 -0300

    Criação e algumas alterações essenciais no menu

--- Dyk2/src/Util/Menu.java
+++ Dyk2/src/Util/Menu.java
@@ -6,27 +6,42 @@ public class Menu {
     
     public void MenuInicializacao()
     {
-        System.out.println("######################################");
-        System.out.println("############ Menu Inicial ############");
-        System.out.println("######################################\n");
+        System.out.println("#####################################");
+        System.out.println("################ DYK ################");
+        System.out.println("#####################################\n");
         
         System.out.println("[1] - Iniciar o jogo");
         System.out.println("[2] - Informações dos desenvolvedores");
         System.out.println("[3] - Explicação do Jogo");
+        System.out.println("[4] - Cadastrar novas perguntas!");
         System.out.println("[0] - Sair\n");
     } 
     public void MenuInicial()
     {
-    
-    }
-    public void MenuInformacoes()
-    {
-    
+        System.out.println("######################################");
+        System.out.println("############ Menu Inicial ############");
+        System.out.println("######################################\n");
+        
+        System.out.println("[1] - Logar");
+        System.out.println("[2] - Cadastrar");
+        System.out.println("[3] - Ranking");
+        System.out.println("[0] - Sair\n");
     }
     public void MenuRegras()
     {
-        System.out.println("Regra do Jogo: \n\n");
-        System.out.print("-->Necess\u00e1rio dois jogadores;\n" + "\n" + "-->Um personagem dever\u00e1 ser escolhido para cada jogador;\n" + "\n" + "-->Ambos iniciam com 100% de vida;\n" + "\n" + "-->Cada pergunta respondida incorretamente, ser\u00e1 debitado o total de 5% chegando a zero;\n" + "\n" + "-->A qualquer momento os jogadores podem solicitar o encerramento da partida\n" + "  ->Caso o outro jogador n\u00e3o esteja de acordo com o t\u00e9rmino do jogo 15% de vida do jogador solicitante ser\u00e1 deduzido\n" + "  ->Esse ser\u00e1 repassado para o outro jogador\n" + "\n" + "-->em seguida a partida \u00e9 encerrada e verifica quem \u00e9 o vencedor\n" + "\n" + "-->O primeiro jogador a atingir 0% de vida ser\u00e1 o perdedor da partida;\n" + "\n" + "-->Vence o jogador que permanecer com a maior vida;");
+        System.out.println("#####################################");
+        System.out.println("########### Regra do jogo ###########");
+        System.out.println("#####################################\n");
+        
+        System.out.print("-->Necessário dois jogadores;\n" + 
+                "\n" + "-->Um personagem deverá ser escolhido para cada jogador;\n" + 
+                "\n" + "-->Ambos iniciam com 100% de vida;\n" + 
+                "\n" + "-->Cada pergunta respondida incorretamente, será debitado o total de 5% chegando a zero;\n" + 
+                "\n" + "-->A qualquer momento os jogadores podem solicitar o encerramento da partida\n" + 
+                "  ->Caso o outro jogador não esteja de acordo com o término do jogo 15% de vida do jogador solicitante será deduzido\n" + 
+                "  ->Esse será repassado para o outro jogador\n" + "\n" + "-->em seguida a partida é encerrada e verifica quem é o vencedor\n" + 
+                "\n" + "-->O primeiro jogador a atingir 0% de vida será o perdedor da partida;\n" + "\n" + 
+                "-->Vence o jogador que permanecer com a maior vida;");
         
         
         try
@@ -36,9 +51,9 @@ public class Menu {
     }
     public void MenuEncerramento()
     {
-        System.out.println("######################################");
-        System.out.println("############ FIM DE JOGO #############");
-        System.out.println("######################################\n");    
+        System.out.println("#####################################");
+        System.out.println("############ FIM DE JOGO ############");
+        System.out.println("#####################################\n");    
         
     }
     
