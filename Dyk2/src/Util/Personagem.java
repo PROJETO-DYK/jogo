6ec75ca612ff6fc320c81e0cf11c61cf9e24faa3
GladsonAmeno <86319539+GladsonAmeno@users.noreply.github.com>
2021-12-08 16:35:01 -0300

    Update Classe de Personagem

--- Dyk2/src/Util/Personagem.java
+++ Dyk2/src/Util/Personagem.java
@@ -5,6 +5,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import static dyk2.Dyk2.clearConsole;
+
 
 public class Personagem
 {
@@ -94,15 +96,20 @@ public class Personagem
 
         personagens = conector.buscarPersonagens();
 
-        System.out.println("Escolha o pesonagem:");
+        System.out.println("Escolha o pesonagem: \n LEMBRANDO QUE DEVE APENAS ESCOLHER AS OPÇÕES NUMERICAS ABAIXO");
         for (Personagem personagem : personagens)
         {
             System.out.println(personagem.getIdPersonagem() + " - " + personagem.getNomePersonagem());
         }
 
         int personagemEscolhido = in.nextInt();
-
+        
+        clearConsole();
+        System.out.println("Opção escolhida: " + personagens.get(personagemEscolhido - 1).getIdPersonagem());
+        System.out.println("O personagem escolhido foi " + personagens.get(personagemEscolhido -1).getNomePersonagem());
+        System.out.println("O personagem tem: " + personagens.get(personagemEscolhido -1).getTempoVida() + " de vida!");
+        System.out.println("O personagem possui a Habilidade: " + personagens.get(personagemEscolhido -1).getHabilidades());
+        
         return personagens.get(personagemEscolhido - 1);
     }
-
 }
